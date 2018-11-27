1490bf922cc3dc1bfb3870c4233c4b85e933d1bd
prokeds <kdmmikata44@gmail.com>
2018-11-27 18:57:01 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  #get 'topics/new'
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
+end
